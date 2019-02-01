a5786121c9ac1d80b981e6e247b8b0b6f48ea14d
Nishad00 <nishadpatil63@gmail.com>
2019-02-01 02:14:04 -0500

    App,View,Urls

---
+++ SNS/blog/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse('<h1>Blog Home</h1>')
+
+def about(request):
+    return HttpResponse('<h1>Blog about</h1>')
