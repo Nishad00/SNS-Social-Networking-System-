be0b0df8aa0ea01cedd51a092f825812df233aa3
Nishad00 <nishadpatil63@gmail.com>
2019-02-09 15:15:53 -0500

    Templates

--- SNS/blog/views.py
+++ SNS/blog/views.py
@@ -1,8 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
+posts = [
+    {
+        'author' : 'Nishad',
+        'title' : 'blog post 1',
+        'content':'first post',
+        'date_posted' :'March 19, 2019'
+    },
+    {
+        'author' : 'rashmi',
+        'title' : 'blog post 2',
+        'content':'second post',
+        'date_posted' :'March 19, 2019'
+    }
+]
+
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+
+    context = {
+        'posts':posts
+    }
+    return render(request,'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>Blog about</h1>')
+    return render(request,'blog/about.html',{'title':'about'})
