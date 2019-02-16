cf65fc30ed4bdfc45b12683ffee00365b66f36f1
Nishad00 <nishadpatil63@gmail.com>
2019-02-16 12:02:27 -0500

    profile_update&blog_image

--- SNS/users/views.py
+++ SNS/users/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, redirect
 #from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
-from .forms import UserRegisterForm  
 from django.contrib.auth.decorators import login_required
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm  
 
 
 def register(request):
@@ -20,4 +20,26 @@ def register(request):
 
 @login_required
 def profile(request):
-    return render(request, 'users/profile.html')
+
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance = request.user)
+        p_form = ProfileUpdateForm(request.POST, request.FILES, instance = request.user.profile)
+
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+
+            messages.success(request,f'Hello! Your Account has been Updated  ')
+            return redirect('profile')
+
+    else:
+        u_form = UserUpdateForm(instance = request.user)
+        p_form = ProfileUpdateForm(instance = request.user.profile)
+
+
+    context = {
+        'u_form':u_form,
+        'p_form':p_form
+    }
+
+    return render(request, 'users/profile.html', context)
